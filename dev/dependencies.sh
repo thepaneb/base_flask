2bd71c634e09132eaba3b9b76705589847a15381
Gil Cleber Barboza <gil.cleber@gmail.com>
2026-02-17 23:00:40 -0300

    atualizações

--- dev/dependencies.sh
+++ dev/dependencies.sh
@@ -1,2 +1,3 @@
+pip install --upgrade pip
 pip list -o | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 pip install -U
 pip install -r app/requirements.txt --upgrade --upgrade-strategy eager
